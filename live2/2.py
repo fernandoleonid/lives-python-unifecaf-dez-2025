94c39aaa175c7362f47d8ba8e3dfa9974245a7b1
Fernando Leonid <fernandoleonid@gmail.com>
2025-12-10 19:46:36 -0300

    exemplo de string com variáveis

---
+++ live2/2.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+idade = input ("Digite sua idade: ")
+cidade = input ("Digite sua cidade: ")
+
+mensagem = f"Olá {nome}! Você tem {idade} anos e mora na cidade de {cidade}"
+
+print (mensagem)
