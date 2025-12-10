4863729506232dc39f2c14ddb2413cff51fa4997
Fernando Leonid <fernandoleonid@gmail.com>
2025-12-10 20:21:05 -0300

    exemplo de estrutura de repetição

---
+++ live2/5.py
@@ -0,0 +1,7 @@
+nome = input ("Digite seu nome: ")
+quantidade = int(input("Digite a quantidade de repetições: "))
+
+contador = 1
+while (contador <= quantidade):
+    print (nome)
+    contador += 1
